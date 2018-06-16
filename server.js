ff382d418f69ca8c8d0f7d21dfaca0e7ad3c34dd
Igor Rudenko <fac30ff@gmail.com>
2018-06-16 11:31:14 +0300

    asd

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 let express = require('express');
 let app = express();
 
+const data = require("./data/fakeData");
+
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(__dirname));
 
 app.get('/api/ping', (req, res) => {
    res.json({
-       "ping": new Date().toISOString()
+       "ping": new Date().toISOString(),
+       "param": req.query.id
    });
+});
+
+app.get('/api/data', (req, res) => {
+    res.json(data);
 });
 
 app.listen(app.get('port'), () => {
