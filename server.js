588e14520ee904cb4338881419c90f05531da7b9
Igor Rudenko <fac30ff@gmail.com>
2018-06-16 11:42:19 +0300

    asd

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ let express = require('express');
 let app = express();
 
 const data = require("./data/fakeData");
+const version = require("./package");
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(__dirname));
+
+app.get('/api/version', (req, res) => {
+    res.json({
+        "version": version.version,
+        "author": version.author
+    });
+});
 
 app.get('/api/ping', (req, res) => {
    res.json({
